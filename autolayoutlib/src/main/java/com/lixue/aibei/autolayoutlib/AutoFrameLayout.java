a3f997cc99736ddc543a7ffee0edd972e10afb89
xiaoxue <lixue100000@sina.com>
2016-02-22 16:51:30 +0800

    常用布局文件的配置

--- autolayoutlib/src/main/java/com/lixue/aibei/autolayoutlib/AutoFrameLayout.java
+++ autolayoutlib/src/main/java/com/lixue/aibei/autolayoutlib/AutoFrameLayout.java
@@ -1,7 +1,10 @@
 package com.lixue.aibei.autolayoutlib;
 
+import android.annotation.TargetApi;
 import android.content.Context;
+import android.os.Build;
 import android.util.AttributeSet;
+import android.view.ViewGroup;
 import android.widget.FrameLayout;
 
 import com.lixue.aibei.autolayoutlib.utils.AutoLayoutHelper;
@@ -11,6 +14,7 @@ import com.lixue.aibei.autolayoutlib.utils.AutoLayoutHelper;
  */
 public class AutoFrameLayout extends FrameLayout{
     private final AutoLayoutHelper mHelper = new AutoLayoutHelper(this);
+
     public AutoFrameLayout(Context context) {
         super(context);
     }
@@ -23,4 +27,65 @@ public class AutoFrameLayout extends FrameLayout{
         super(context, attrs, defStyleAttr);
     }
 
+    @TargetApi(Build.VERSION_CODES.LOLLIPOP)
+    public AutoFrameLayout(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+        super(context, attrs, defStyleAttr, defStyleRes);
+    }
+
+    @Override
+    public LayoutParams generateLayoutParams(AttributeSet attrs) {
+        return new LayoutParams(getContext(),attrs);
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        if (!isInEditMode())
+            mHelper.adjustChildren();
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+    }
+
+    @Override
+    protected void onLayout(boolean changed, int left, int top, int right, int bottom) {
+        super.onLayout(changed, left, top, right, bottom);
+    }
+
+    private static class LayoutParams extends FrameLayout.LayoutParams implements AutoLayoutHelper.AutoLayoutParams{
+        private AutoLayoutInfo mInfo;
+
+        public LayoutParams(Context c, AttributeSet attrs) {
+            super(c, attrs);
+            mInfo = AutoLayoutHelper.getAutoLayoutInfo(c,attrs);
+        }
+
+        public LayoutParams(int width, int height) {
+            super(width, height);
+        }
+
+        public LayoutParams(int width, int height, int gravity) {
+            super(width, height, gravity);
+        }
+
+        public LayoutParams(ViewGroup.LayoutParams source) {
+            super(source);
+        }
+
+        public LayoutParams(MarginLayoutParams source) {
+            super(source);
+        }
+
+        public LayoutParams(FrameLayout.LayoutParams source) {
+            super((MarginLayoutParams)source);
+            gravity = source.gravity;
+        }
+
+        public LayoutParams(LayoutParams source){
+            this((FrameLayout.LayoutParams) source);
+            mInfo = source.mInfo;
+        }
+
+        @Override
+        public AutoLayoutInfo getAutoLayoutInfo() {
+            return mInfo;
+        }
+    }
 }
--- autolayoutlib/src/main/java/com/lixue/aibei/autolayoutlib/AutoRelativeLayout.java
+++ autolayoutlib/src/main/java/com/lixue/aibei/autolayoutlib/AutoRelativeLayout.java
@@ -1,7 +1,81 @@
 package com.lixue.aibei.autolayoutlib;
 
+import android.annotation.TargetApi;
+import android.content.Context;
+import android.os.Build;
+import android.util.AttributeSet;
+import android.view.ViewGroup;
+import android.widget.RelativeLayout;
+
+import com.lixue.aibei.autolayoutlib.utils.AutoLayoutHelper;
+
 /**
  * Created by Administrator on 2015/12/31.
  */
-public class AutoRelativeLayout {
+public class AutoRelativeLayout extends RelativeLayout{
+    private AutoLayoutHelper helper = new AutoLayoutHelper(this);
+
+    public AutoRelativeLayout(Context context) {
+        super(context);
+    }
+
+    public AutoRelativeLayout(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public AutoRelativeLayout(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    @TargetApi(Build.VERSION_CODES.LOLLIPOP)
+    public AutoRelativeLayout(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+        super(context, attrs, defStyleAttr, defStyleRes);
+    }
+
+    @Override
+    public LayoutParams generateLayoutParams(AttributeSet attrs) {
+        return new LayoutParams(getContext(),attrs);
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        if (!isInEditMode())
+            helper.adjustChildren();
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+    }
+
+    @Override
+    protected void onLayout(boolean changed, int l, int t, int r, int b) {
+        super.onLayout(changed, l, t, r, b);
+    }
+
+    public static class LayoutParams extends RelativeLayout.LayoutParams implements AutoLayoutHelper.AutoLayoutParams{
+        private AutoLayoutInfo mAutoLayoutInfo;
+
+        public LayoutParams(Context c, AttributeSet attrs) {
+            super(c, attrs);
+            mAutoLayoutInfo = AutoLayoutHelper.getAutoLayoutInfo(c, attrs);
+        }
+
+        public LayoutParams(int w, int h) {
+            super(w, h);
+        }
+
+        public LayoutParams(ViewGroup.LayoutParams source) {
+            super(source);
+        }
+
+        public LayoutParams(MarginLayoutParams source) {
+            super(source);
+        }
+
+        public LayoutParams(RelativeLayout.LayoutParams source) {
+            super(source);
+        }
+
+        @Override
+        public AutoLayoutInfo getAutoLayoutInfo() {
+            return mAutoLayoutInfo;
+        }
+    }
 }
